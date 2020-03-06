5a75e1bbb1e5e6b907950abec3da7e1496479654
dgujs <jinsoo8457@dongguk.edu>
2020-03-06 20:44:32 +0900

    Query_add

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import { HOME_PAGE } from './queries';
 
-const Home = () => "Home";
+const Home = () => <Query qeury={HOME_PAGE}>{({ loading, data, error }) => {
+    if (loading) return <span> loading </span>
+    if (error) return <span> error </span>
+    return data.movies.map(movie => (<h2 key={movie.id}>{movie.title} / {movie.rating}</h2>
+    ));
+
+}}</Query>;
 
 export default Home
